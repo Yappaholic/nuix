c2ffff8c6a5ea94676d587a0f88c534d10f2a492
Yappaholic <sav.boyar@gmail.com>
2025-06-15 16:23:50 +0300

    Add version and nh option

--- main.go
+++ main.go
@@ -6,23 +6,33 @@ import (
 	"strings"
 )
 
+const version = "0.0.1"
+
 type Application struct {
-	args []string
+	args     []string
+	preferNH bool
 }
 
-var app = Application{}
+var app = Application{preferNH: false}
 
 var commandsGlobal = []Pair[string, string]{
-	CPair("name", "Comfiness of guix cli for nix/NixOS users"),
+	CPair("name", "Nuix - Comfiness of guix cli for nix/NixOS users"),
 	CPair("usage", "nuix [COMMAND] [OPTIONS]"),
 	CPair("system", "NixOS related commands"),
 	CPair("home", "Configure your home"),
 	CPair("gc", "Clean your nix store"),
 	CPair("search", "Search for packages"),
-	CPair("package", "Manage installed nix packages(not recommended)"),
+	CPair("package", "Manage and install nix packages(not recommended)"),
 	CPair("channels", "Manage nix channels"),
 	CPair("shell", "Create a new shell environment"),
 	CPair("download", "Fetch source and get hash output"),
+}
+
+var optionsGlobal = []Pair[string, string]{
+	CPair("-h|--help", "Get help info for current command"),
+	CPair("--man", "Open mandoc for current command"),
+	CPair("--version", "Get version of this program"),
+	CPair("--nh", "Prefer using Nix Helper for commands"),
 }
 
 func formatOptions(commands []Pair[string, string]) {
@@ -37,7 +47,9 @@ func showHelp(args []string) {
 	var commands []Pair[string, string]
 	var name any
 	var usage any
-	if len(args) > 1 && args[0] != "-h" && args[1] != "--help" {
+	if len(args) > 1 &&
+		strings.HasPrefix(args[0], "-") == false &&
+		strings.HasPrefix(args[0], "--") == false {
 		switch args[0] {
 		case "system":
 			commands = commandsSystem
@@ -59,6 +71,8 @@ func showHelp(args []string) {
 	fmt.Printf("Usage: %s\n", usage)
 	fmt.Println("Available commands:")
 	formatOptions(commands)
+	fmt.Println("Available options:")
+	formatOptions(optionsGlobal)
 	os.Exit(0)
 }
 
@@ -69,9 +83,28 @@ func isHelp(args []string) {
 		}
 	}
 }
+func isVersion(args []string) {
+	for _, v := range args {
+		if v == "--version" {
+			fmt.Println("Nuix version ", version)
+			os.Exit(0)
+		}
+	}
+
+}
+
+func isNH(args []string) {
+	for _, v := range args {
+		if v == "--nh" {
+			app.preferNH = true
+		}
+	}
+}
 
 func parseCommands(args []string) {
 	isHelp(args)
+	isVersion(args)
+	isNH(args)
 	if len(args) == 0 {
 		notSpecifiedGlobal()
 	}
